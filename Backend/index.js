730585dfc74c1302387339f3004707d07589c94e
Ravindra Yadav <yadavra2005@gmail.com>
2024-07-30 20:51:36 +0530

    setup socket

--- Backend/index.js
+++ Backend/index.js
@@ -1,16 +1,35 @@
 import express from "express";
 import 'dotenv/config'
+import { Server } from 'socket.io';
 
 const app = express();
 
 const port = process.env.PORT || 8080;
 
+
+app.get('/', (req, res) => {
+  res.send("Chat with ChatterAi");
+});
+
 // console.log(process.env);
+
+const server = app.listen(port, () => {
+    console.log(`app is live in port ${port}`);
+})
+
+const io = new Server(server);
+
 
 app.get("/", (req, res) => {
     res.send("Chat with chatter");
 })
 
-app.listen(port, () => {
-    console.log(`app is live in port ${port}`);
-})
+
+// socket.io setup
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+  });
+
