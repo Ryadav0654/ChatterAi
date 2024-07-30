89feba463e1b5e7ca0a8fb304f0f831f36f91381
Ravindra Yadav <yadavra2005@gmail.com>
2024-07-30 20:52:17 +0530

    ChatterAi

--- Fronted/src/App.jsx
+++ Fronted/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
      <h1 className='text-4xl font-bold text-blue-500'>Chat with ChatterAi </h1>
+     
     </>
   )
 }
